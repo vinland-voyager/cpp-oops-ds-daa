8ae1c663567043393fcf612b32592c0e411dc3b3
aayush <akjindividual@gmail.com>
2025-12-15 22:23:19 +0530

    Day 2: Learned & Practiced Pointers

--- Practice.cpp
+++ Practice.cpp
@@ -1,3 +1,20 @@
+/* Pointers -------------------------------
+
+#include <iostream>
+using namespace std;
+#define NULL nullptr
+
+int main() {
+    int * p1 {nullptr}, p2 {10}, p3 {0}, * p4 {&p2}, * p5 {NULL}, *p6 {NULL};
+    cout << p4 << endl; // Will print the address of entity p2.
+    cout << *p4 << endl; // Will print the value stored at variable p2.
+    *p4 = 100; // changes the value of p2 to 10.
+    cout << p4 << endl; // It will still print the same address.
+    cout << *p4 << endl; // It will still print the value stored at p2 but with the updated value.
+    return 0;
+}
+ */
+
 /* Uniform Initialization -------------------------------
 
 #include <iostream>
@@ -22,4 +39,3 @@ objX obj1(1); // How is it different than a function prototype?
 objX obj2{1}; // This clearly is.
 
 */
-
